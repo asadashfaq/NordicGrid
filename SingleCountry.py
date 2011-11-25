dc23e7a9519eb03b9a0a247d3e5691a0aae831aa
Gorm Bruun Andresen <gba@ase.au.dk>
2011-11-25 11:34:54 +0100

    Can now reproduce solarDK plot

--- SingleCountry.py
+++ SingleCountry.py
@@ -13,111 +13,246 @@ import os
 import sys
 
 #Custom functions
-import shortcuts
+from shortcuts import *
 from Database_v1 import get_data_countries, get_data_regions
 from MortenStorage import get_policy_2_storage
 
+#Specific functions, not sure if alla are actually used
+from scipy.optimize import brentq
+from scipy.optimize import fmin
+from scipy.optimize import leastsq
+from scipy.interpolate import Rbf
 
 
+##Colors, should be moved to a color module
+color_wind = (0.5,0.7,1.)
+color_solar = (1.,.8,0.)
+bg_color = (.75,.0,.0)
 
-    
 ###
-# DK: i=7; plot_country_optimal_mix_vs_gamma(L[i], GW[i], GS[i], gamma=linspace(0,2.05,31))
-# DK, 6h storage:  i=7; plot_country_optimal_mix_vs_gamma(L[i], GW[i], GS[i], gamma=linspace(0,2.05,31),CS=6)
+# plot_country_optimal_mix_vs_gamma('DK', gamma=linspace(0,2.05,31))
+# 6h storage: plot_country_optimal_mix_vs_gamma('DK', gamma=linspace(0,2.05,31),CS=6)
 #
 #(1.,.53,.20)
-def plot_country_optimal_mix_vs_gamma(L, GW, GS, gamma=linspace(0,2,11),p_interval=[0.01,0.05,0.25], CS=None,linespec=['--','-.',':'],color=[(0.53,0.73,0.37),(1.,.82,.20),(.90,.27,.20)]):
-	
+def plot_country_optimal_mix_vs_gamma(ISO='DK', gamma=linspace(0,2.05,11), p_interval=[0.01,0.05,0.25], CS=None, linespec=['--','-.',':'], color=[(0.53,0.73,0.37),(1.,.82,.20),(.90,.27,.20)]):
+    """Legacy function from solarDK.py. Should be updated at some point."""
+
+    #Load data
+    t, L, Gw, Gs, datetime_offset, datalabel = get_ISET_country_data(ISO)
+
+    alpha_w_opt, alpha_w_opt_1p_interval, res_load_sum_opt, res_load_sum_1p = zeros(len(gamma)), zeros((len(gamma),2)), zeros(len(gamma)), zeros(len(gamma))
+    lower_bound, upper_bound, res_load_sum_p = zeros((len(p_interval),len(gamma))), zeros((len(p_interval),len(gamma))), zeros((len(p_interval),len(res_load_sum_1p)))
+    for j in arange(len(p_interval)):
+        for i in arange(len(gamma)):
+            alpha_w_opt[i], alpha_w_opt_1p_interval[i], res_load_sum_opt[i], mismatch_opt, res_load_sum_1p[i] = get_optimal_mix_balancing(L, Gw, Gs, gamma[i], p_interval=p_interval[j], CS=CS, returnall=True, normalized=True)
+
+        lower_bound[j] = alpha_w_opt_1p_interval.transpose()[0]
+        upper_bound[j] = alpha_w_opt_1p_interval.transpose()[1]
+        res_load_sum_p[j] = res_load_sum_1p/len(L)
+
+    #mask = find((lower_bound[0]!=0) + (upper_bound[0]!=1))
+    mask = arange(len(gamma) - amax([argmin(lower_bound[0][::-1]),argmax(upper_bound[0][::-1])]),len(gamma))
+
+    res_load_sum_opt = res_load_sum_opt/len(L)
+
+    #Set plot options	
+    matplotlib.rcParams['font.size'] = 10
+
+    figure(1); clf()
+
+    gcf().set_dpi(300)
+    gcf().set_size_inches([4.75,6])
+
+    #Upper panel
+    ax1 = axes([.11,.565,.885,.42])
+    plot(gamma[mask],alpha_w_opt[mask],'w-',lw=2)
+
+    fill_between(gamma,ones(len(gamma)),color=bg_color,edgecolor=(0,0,0,0))
+
+    pp = list(zeros(1+len(p_interval)))
+    for j in arange(len(p_interval))[::-1]:
+        fill_between(gamma,lower_bound[j],upper_bound[j],color=color[j],lw=1,edgecolor=(0,0,0,0))
+        pp[j] = Rectangle((0, 0), 1, 1, color=color[j],lw=1)
+        
+        plot(gamma,lower_bound[j],linespec[j],color='w',lw=2)
+        plot(gamma,upper_bound[j],linespec[j],color='w',lw=2)
+
+    axvline(.2,ls='--',color='k')
+    #plot(gamma,(0.2 + (gamma-0.2)*0.75)/gamma,'k--')
+
+    pp[-1] = Rectangle((0, 0), 1, 1, color=bg_color,lw=1)
+    pp = list(pp)
+
+    axis(xmin=0,xmax=amax(gamma),ymin=0,ymax=1)
+    xlabel(r'Average share of electricity demand $\gamma_{'+ISO+'}$')
+    ylabel(r'Wind fraction $\alpha_W$')
+
+    txtlabels = ['0-1 pp','1-5 pp','5-25 pp','>25 pp']
+
+    leg = legend(pp,txtlabels,loc='lower right',ncol=2);
+    ltext  = leg.get_texts();
+    setp(ltext, fontsize='small')    # the legend text fontsize
+
+
+    #Lower panel
+    ax2 = axes([.11,.065,.885,.42])
+
+    plot([0,1],[1,0],'-',color='k',alpha=.3)
+
+    pp_solar = plot(gamma,get_balancing(L, Gw, Gs, gamma, CS=CS, alpha=0.)[0]/len(L),'-',color=color_solar,lw=2)
+    pp_wind = plot(gamma,get_balancing(L, Gw, Gs, gamma,  CS=CS, alpha=1.)[0]/len(L),'-',color=color_wind,lw=2)
+
+    pp = list(zeros(len(p_interval)))
+    for j in arange(len(p_interval))[::-1]:
+        pp_ = plot(gamma,res_load_sum_p[j],linespec[j],color='k')
+        pp[j] = pp_[0]
+        
+    pp_opt = plot(gamma,res_load_sum_opt,'k-')
+
+    axvline(.2,ls='--',color='k')
+
+                
+    axis(xmin=0,xmax=amax(gamma),ymin=0,ymax=1)
+    ylabel('Av. residual load [av.h.l.]')
+    xlabel(r'Average share of electricity demand $\gamma_{'+ISO+'}$')
+
+    pp2 = [pp_opt[0],pp_wind[0],pp_solar[0]]
+    pp2.extend(pp)
+    txtlabels = ['Optimal mix','Wind only','Solar only','1 pp contour','5 pp contour','25 pp contour']
+
+    leg = legend(pp2,txtlabels,loc='upper right',ncol=2);
+    ltext  = leg.get_texts();
+    setp(ltext, fontsize='small')    # the legend text fontsize
+
+    save_file_name = 'plot_country_optimal_mix_vs_gamma_'+'CS_'+str(CS)+'.png'
+    save_figure(save_file_name)
+    
+
+###############
+### Tools: ####
+###############
+
+def get_optimal_mix_storage(L, GW, GS, gamma=1., p_interval=0.01, returnall=False):
+
 	L, GW, GS = array(L,ndmin=2), array(GW,ndmin=2), array(GS,ndmin=2)  #Ensure minimum dimension to 2 to alow the weighed sum to be calculated correctly.
+	weighed_sum = lambda x: sum(x,axis=0)/mean(sum(x,axis=0))
 
-	bg_color = (.75,.0,.0)
+	l = weighed_sum(L)
+	Gw = weighed_sum(GW)	
+	Gs = weighed_sum(GS)
 
-	alpha_w_opt, alpha_w_opt_1p_interval, res_load_sum_opt, res_load_sum_1p = zeros(len(gamma)), zeros((len(gamma),2)), zeros(len(gamma)), zeros(len(gamma))
-	lower_bound, upper_bound, res_load_sum_p = zeros((len(p_interval),len(gamma))), zeros((len(p_interval),len(gamma))), zeros((len(p_interval),len(res_load_sum_1p)))
-	for j in arange(len(p_interval)):
-		for i in arange(len(gamma)):
-			alpha_w_opt[i], alpha_w_opt_1p_interval[i], res_load_sum_opt[i], mismatch_opt, res_load_sum_1p[i] = get_optimal_mix_balancing(L, GW, GS, gamma[i], p_interval=p_interval[j], CS=CS, returnall=True, normalized=True)
-
-		lower_bound[j] = alpha_w_opt_1p_interval.transpose()[0]
-		upper_bound[j] = alpha_w_opt_1p_interval.transpose()[1]
-		res_load_sum_p[j] = res_load_sum_1p/len(L[0])
+	mismatch = lambda alpha_w: gamma*(alpha_w*Gw + (1.-alpha_w)*Gs) - l
+	E_H = lambda alpha_w: get_policy_2_storage(mismatch(alpha_w))[1]
 	
-	#mask = find((lower_bound[0]!=0) + (upper_bound[0]!=1))
-	mask = arange(len(gamma) - amax([argmin(lower_bound[0][::-1]),argmax(upper_bound[0][::-1])]),len(gamma))
+	alpha_w_opt = fmin(E_H,0.5,disp=True)
 	
-	res_load_sum_opt = res_load_sum_opt/len(L[0])
+	if returnall:
 	
-	#Set plot options	
-	matplotlib.rcParams['font.size'] = 10
+		balancing_fixed_E_H = lambda alpha_w: sum(get_positive(-get_policy_2_storage(mismatch(alpha_w),storage_capacity = E_H(alpha_w_opt))[0])) - p_interval*sum(l)
+	
+		if balancing_fixed_E_H(0)>0:
+			if balancing_fixed_E_H(1)>0:
+				alpha_w_opt_1p_interval = array([brentq(balancing_fixed_E_H, 0, alpha_w_opt),brentq(balancing_fixed_E_H, alpha_w_opt, 1)])
+			else:
+				alpha_w_opt_1p_interval = array([brentq(balancing_fixed_E_H, 0, alpha_w_opt),1.])
+		else:	
+			if balancing_fixed_E_H(1)>0:
+				alpha_w_opt_1p_interval = array([0.,brentq(balancing_fixed_E_H, alpha_w_opt, 1)])
+			else:
+				alpha_w_opt_1p_interval = array([0.,1.])
+	
+		print balancing_fixed_E_H(0), balancing_fixed_E_H(1)
+		print alpha_w_opt, alpha_w_opt_1p_interval
+	
+	
+		#Returns: alpha_w_opt, alpha_w_opt_1p_interval
+		return alpha_w_opt, alpha_w_opt_1p_interval, E_H(alpha_w_opt)
+	else:
+		return E_H(alpha_w_opt), alpha_w_opt
 
-	figure(1); clf()
 
-	gcf().set_dpi(300)
-	gcf().set_size_inches([4.75,6])
+def get_balancing(L, GW, GS, gamma=1, alpha=1., CS=None,returnall=False):
 
-	#Upper panel
-	ax1 = axes([.11,.565,.885,.42])
-	plot(gamma[mask],alpha_w_opt[mask],'w-',lw=2)
+	L, GW, GS, gamma, alpha = array(L,ndmin=2), array(GW,ndmin=2), array(GS,ndmin=2), array(gamma,ndmin=1), array(alpha,ndmin=1)  #Ensure minimum dimension to 2 to alow the weighed sum to be calculated correctly.
+	weighed_sum = lambda x: sum(x,axis=0)/mean(sum(x,axis=0))
+
+	l = weighed_sum(L)
+	Gw = weighed_sum(GW)	
+	Gs = weighed_sum(GS)
 	
-	fill_between(gamma,ones(len(gamma)),color=bg_color,edgecolor=(0,0,0,0))
+	mismatch = lambda alpha_w, gamma: gamma*(alpha_w*Gw + (1.-alpha_w)*Gs) - l
 	
-	pp = list(zeros(1+len(p_interval)))
-	for j in arange(len(p_interval))[::-1]:
-		fill_between(gamma,lower_bound[j],upper_bound[j],color=color[j],lw=1,edgecolor=(0,0,0,0))
-		pp[j] = Rectangle((0, 0), 1, 1, color=color[j],lw=1)
+	if CS==None:
+		res_load_sum = lambda alpha_w, gamma: sum(get_positive(-mismatch(alpha_w,gamma)))
+	else:
+		res_load_sum = lambda alpha_w, gamma: sum(get_positive(-get_policy_2_storage(mismatch(alpha_w,gamma),storage_capacity = CS)[0]))
+	
+	Gamma, Alpha = meshgrid(gamma,alpha)
+	
+	Res_load_sum = zeros(Gamma.shape)
+	for i in arange(size(Gamma)):
+		Res_load_sum.flat[i] = res_load_sum(Alpha.flat[i],Gamma.flat[i])
+	
+	if returnall:
+		return Res_load_sum, Gamma, Alpha
+	else:
+		return Res_load_sum
+
+
+def get_optimal_mix_balancing(L, GW, GS, gamma=1., p_interval=0.01, CS=None, returnall=False, normalized=True):
+
+	L, GW, GS = array(L,ndmin=2), array(GW,ndmin=2), array(GS,ndmin=2)  #Ensure minimum dimension to 2 to alow the weighed sum to be calculated correctly.
+	weighed_sum = lambda x: sum(x,axis=0)/mean(sum(x,axis=0))
+
+	l = weighed_sum(L)
+	Gw = weighed_sum(GW)	
+	Gs = weighed_sum(GS)
+
+	mismatch = lambda alpha_w: gamma*(alpha_w*Gw + (1.-alpha_w)*Gs) - l
+	
+	if CS==None:
+		res_load_sum = lambda alpha_w: sum(get_positive(-mismatch(alpha_w)))
+	else:
+		res_load_sum = lambda alpha_w: sum(get_positive(-get_policy_2_storage(mismatch(alpha_w),storage_capacity = CS)[0]))
+	
+	alpha_w_opt = fmin(res_load_sum,0.5,disp=False)
+	if alpha_w_opt>1.:
+		alpha_w_opt = 1.
+	elif alpha_w_opt<0.:
+		alpha_w_opt = 0.
+	
+	
+	if normalized:
+		mismatch_opt = mismatch(alpha_w_opt)
+	else:
+		mismatch_opt = mismatch(alpha_w_opt)*mean(sum(L,axis=0))
+	res_load_sum_opt = res_load_sum(alpha_w_opt)
+	
+	if returnall:
+		res_load_sum_1p_interval = lambda alpha_w: res_load_sum(alpha_w)-(res_load_sum(alpha_w_opt)+p_interval*sum(l))
 		
-		plot(gamma,lower_bound[j],linespec[j],color='w',lw=2)
-		plot(gamma,upper_bound[j],linespec[j],color='w',lw=2)
+		if sign(res_load_sum_1p_interval(0))!=sign(res_load_sum_1p_interval(alpha_w_opt)):
+			lower_bound = brentq(res_load_sum_1p_interval, 0, alpha_w_opt)
+		else:
+			lower_bound = 0
+		
+		if sign(res_load_sum_1p_interval(1))!=sign(res_load_sum_1p_interval(alpha_w_opt)):
+			upper_bound = brentq(res_load_sum_1p_interval, alpha_w_opt, 1)
+		else:
+			upper_bound = 1
+		
+		alpha_w_opt_1p_interval = array([lower_bound,upper_bound])
+		res_load_sum_1p = amax([res_load_sum(lower_bound),res_load_sum(upper_bound)])
+		
+		#Returns: alpha_w_opt, alpha_w_opt_1p_interval, res_load_sum_opt, mismatch_opt
+		return alpha_w_opt, alpha_w_opt_1p_interval, res_load_sum_opt, mismatch_opt, res_load_sum_1p
+	else:
+		return alpha_w_opt
 
-	axvline(.2,ls='--',color='k')
-	#plot(gamma,(0.2 + (gamma-0.2)*0.75)/gamma,'k--')
 
-	pp[-1] = Rectangle((0, 0), 1, 1, color=bg_color,lw=1)
-	pp = tuple(pp)
-	
-	axis(xmin=0,xmax=amax(gamma),ymin=0,ymax=1)
-	xlabel(r'RES power generation coefficient $\gamma$')
-	ylabel(r'Wind fraction $\alpha_W$')
-	
-	txtlabels = ('0-1 pp','1-5 pp','5-25 pp','>25 pp')
-	
-	leg = legend(pp,txtlabels,loc='lower right',ncol=2);
-	ltext  = leg.get_texts();
-	setp(ltext, fontsize='small')    # the legend text fontsize
-	
-	
-	#Lower panel
-	ax2 = axes([.11,.065,.885,.42])
-	
-	plot([0,1],[1,0],'-',color='k',alpha=.3)
-	
-	pp_solar = plot(gamma,get_balancing(L, GW, GS, gamma, CS=CS, alpha=0.)[0]/len(L[0]),'-',color=color_solar,lw=2)
-	pp_wind = plot(gamma,get_balancing(L, GW, GS, gamma,  CS=CS, alpha=1.)[0]/len(L[0]),'-',color=color_wind,lw=2)
-	
-	pp = list(zeros(len(p_interval)))
-	for j in arange(len(p_interval))[::-1]:
-		pp_ = plot(gamma,res_load_sum_p[j],linespec[j],color='k')
-		pp[j] = pp_
-	pp = tuple(pp)
-	
-	pp_opt = plot(gamma,res_load_sum_opt,'k-')
 
-	axvline(.2,ls='--',color='k')
-	
-				
-	axis(xmin=0,xmax=amax(gamma),ymin=0,ymax=1)
-	ylabel('Av. residual load [av.h.l.]')
-	xlabel(r'RES power generation coefficient $\gamma$')
-	
-	pp = (pp_opt,pp_wind,pp_solar) + pp
-	txtlabels = ('Optimal mix','Wind only','Solar only','1 pp contour','5 pp contour','25 pp contour')
-	
-	leg = legend(pp,txtlabels,loc='upper right',ncol=2);
-	ltext  = leg.get_texts();
-	setp(ltext, fontsize='small')    # the legend text fontsize
-	
-	save_file_name = 'plot_country_optimal_mix_vs_gamma_'+'CS_'+str(CS)+'.png'
-	save_figure(figname=save_file_name, fig=gcf(), path='./', dpi=300)
+
+
 
 
 ######
@@ -153,7 +288,7 @@ def get_ISET_country_data(ISO='DK',path='./data/'):
         country ISO
         
     Example: Returns the Danish load, wind and solar power generation
-        t, L, GW, GS, datetime_offset, datalabel = get_ISET_country_data('DK')
+        t, L, Gw, Gs, datetime_offset, datalabel = get_ISET_country_data('DK')
     
     """
     
@@ -191,7 +326,7 @@ def get_ISET_country_data(ISO='DK',path='./data/'):
         print 'Loaded file: ', path + filename
         sys.stdout.flush()
         
-    return npzfile['t'], npzfile['L'], npzfile['Gs'], npzfile['Gs'], npzfile['datetime_offset'], npzfile['datalabel']
+    return npzfile['t'], npzfile['L'], npzfile['Gw'], npzfile['Gs'], npzfile['datetime_offset'], npzfile['datalabel']
 
 def valid_ISO(ISO='DK',filename='ISET2ISO_country_codes.npy',path='./settings/'):
 

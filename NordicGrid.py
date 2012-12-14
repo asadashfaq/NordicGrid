948351de3bf8197191848f5e6d06e4e7baa2473d
Gorm Bruun Andresen <gba@ase.au.dk>
2012-12-14 15:44:01 +0100

    Progress

--- NordicGrid.py
+++ NordicGrid.py
@@ -51,7 +51,9 @@ def test_new_flow_calc(lapse=None):
 #
 #   year, data_nodes, data_flows = get_nodes_and_flows_vs_year(year=linspace(1985,2053,5),lapse=365*24,path_nodes='./output_data/test/')
 #
-def get_nodes_and_flows_vs_year(year=linspace(1985,2053,21), add_color=False,path_nodes='./output_data/',admat='./settings/admat_2011.txt',path_data='./data/',files_data=['ISET_NordicGrid_DE-N.npz', 'ISET_NordicGrid_DK-W.npz', 'ISET_NordicGrid_SE.npz','ISET_NordicGrid_DK-E.npz', 'ISET_NordicGrid_NO.npz'], path_settings='./settings/',copper=0, h0=None, b=1.0, lapse=None, squaremin=False, maxb=True):
+#   year, data_nodes, data_flows = get_nodes_and_flows_vs_year(year=linspace(1985,2053,21),lapse=365*24,path_nodes='./output_data/test/')
+#
+def get_nodes_and_flows_vs_year(year=linspace(1985,2053,21), add_color=False,path_nodes='./output_data/',admat='./settings/admat_2011.txt',path_data='./data/',files_data=['ISET_NordicGrid_NO.npz', 'ISET_NordicGrid_SE.npz', 'ISET_NordicGrid_DK-W.npz', 'ISET_NordicGrid_DK-E.npz', 'ISET_NordicGrid_DE-N.npz'], path_settings='./settings/',copper=0, h0=None, b=1.0, lapse=None, squaremin=False, maxb=True):
     
     """ (almost) Updated to use EuropeanGridR. Colors are not yet working."""
 
@@ -101,13 +103,11 @@ def get_nodes_and_flows_vs_year(year=linspace(1985,2053,21), add_color=False,pat
 
     return year, data_nodes, data_flows
 
-#
-# plot_generation_summary_vs_year(year,data_nodes,lapse=50*24)
-#
-# plot_generation_summary_vs_year(year_cu,data_nodes_cu,lapse=None,datalabel='Copper')
+# plot_generation_summary_vs_year(year, data_nodes,lapse=365*24,datalabel='Test')
 #
 def plot_generation_summary_vs_year(year,data,lapse=50*24,datalabel=None):
-
+    """ (almost) Updated to use EuropeanGridR. Colors are not yet working."""
+    
     if datalabel != None:
         datalabel = datalabel + '_'
 
@@ -170,8 +170,7 @@ def plot_generation_summary_vs_year(year,data,lapse=50*24,datalabel=None):
 
 ##New version
 #
-# plot_generation_summary_vs_year_2(year,data_nodes,node_id=[2,3],lapse=None,datalabel='Denmark')
-# plot_generation_summary_vs_year_2(year_cu,data_nodes_cu,node_id=[2,3],lapse=None,datalabel='Denmark_copper')
+
 # 
 def plot_generation_summary_vs_year_2(year,data_nodes,node_id=[2,3],lapse=50*24,datalabel='Denmark'):
 

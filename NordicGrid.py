2db3e5951e290bee9f69ef8a6c06f646980ac667
Gorm Bruun Andresen <gba@ase.au.dk>
2012-12-14 14:57:55 +0100

    Added basepath data files

--- NordicGrid.py
+++ NordicGrid.py
@@ -48,6 +48,9 @@ def test_new_flow_calc(lapse=None):
     
     return N, F
 
+#
+#   year, data_nodes, data_flows = get_nodes_and_flows_vs_year()
+#
 def get_nodes_and_flows_vs_year(year=linspace(1985,2053,21), add_color=False,path_nodes='./output_data/',admat='./settings/admat_2011.txt',path_data='./data/',files_data=['ISET_NordicGrid_DE-N.npz', 'ISET_NordicGrid_DK-W.npz', 'ISET_NordicGrid_SE.npz','ISET_NordicGrid_DK-E.npz', 'ISET_NordicGrid_NO.npz'], copper=0, h0=None, b=1.0, lapse=None, squaremin=False, maxb=True):
     
     """ (almost) Updated to use EuropeanGridR. Colors are not yet working."""

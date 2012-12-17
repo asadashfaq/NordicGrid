db5b3d867f89ca34ca9fcc99bb3b92e5868807b7
Gorm Bruun Andresen <gba@ase.au.dk>
2012-12-17 19:58:46 +0100

    Hopefully fixed GurobiError for some apparently random gammas.

--- NordicGrid.py
+++ NordicGrid.py
@@ -97,9 +97,11 @@ def get_nodes_and_flows_vs_year(year=linspace(1985,2053,21), add_color=False,pat
             
             #Calculate flows
             
-            N,F = aures_solve(N, path=path_settings, copper=copper, h0=h0, b=b, lapse=lapse, squaremin=squaremin, maxb=maxb)
-            #GurobiError
-            #### DOES NOT WORK YET!!!
+            try:
+                N,F = aures_solve(N, path=path_settings, copper=copper, h0=h0, b=b, lapse=lapse, squaremin=squaremin, maxb=maxb)
+            except GurobiError:
+                N.set_gammas(Gamma.transpose()[i] + 0.01*rand(len(Gamma.transpose()[i])))
+            
             if add_color:
                 #N.add_colored_import(F,lapse=lapse)
                 N = track_imports(N,F,admat=admat,lapse=lapse)
